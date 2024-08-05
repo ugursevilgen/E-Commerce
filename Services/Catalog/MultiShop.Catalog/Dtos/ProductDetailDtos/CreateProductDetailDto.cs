1dcee888cd0f2c45be23f8d47c9cb9d3b3526d37
Uğur Sevilgen <ugursevilgen@gmail.com>
2024-08-05 16:24:53 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/CreateProductDetailDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDetailDtos
+{
+    public class CreateProductDetailDto
+    {      
+
+        public string ProductDescription { get; set; }
+
+        public string ProductInfo { get; set; }
+
+
+        public string ProductId { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/UpdateProductDetailDto.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDetailDtos
+{
+    public class UpdateProductDetailDto
+    {
+        public string ProductDetailID { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public string ProductInfo { get; set; }
+
+
+        public string ProductId { get; set; }
+    }
+}
